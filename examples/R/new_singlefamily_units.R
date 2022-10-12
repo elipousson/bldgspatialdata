52791b2e746e52b7326643b7320add5b7a82043c
Eli Pousson <eli.pousson@gmail.com>
2022-10-12 18:04:37 -0400

    feat: add tidying data exercise

---
+++ examples/R/new_singlefamily_units.R
@@ -0,0 +1,27 @@
+new_singlefamily_units <-
+  suppressMessages(
+    readr::read_csv(
+      "https://opendata.maryland.gov/api/views/4br4-qbf4/rows.csv"
+    )
+  )
+
+new_singlefamily_units_long <-
+  tidyr::pivot_longer(
+    new_singlefamily_units,
+    cols = c(dplyr::contains("County"), dplyr::contains("City")),
+    names_to = "county",
+    values_to = "num_units"
+  )
+
+new_singlefamily_units_long <-
+  new_singlefamily_units_long %>%
+  dplyr::mutate(
+    county = dplyr::case_when(
+      county == "Baltimore City" ~ "Baltimore city",
+      TRUE ~ county
+    )
+  ) %>%
+  dplyr::select(-c(`Date created`, MARYLAND))
+
+# ggplot(data = filter(md_housing_units_long, county == "Baltimore city")) +
+#   geom_col(aes(x = Year, y = num_units, fill = type), position = "dodge")
